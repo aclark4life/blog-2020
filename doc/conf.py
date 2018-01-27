ab27f2f45e8ba92901436a039a0b55336acdd75f
Alex Clark <aclark@aclark.net>
2018-01-26 19:00:47 -0500

    Update

--- doc/conf.py
+++ doc/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = "Alex Clark's Blog"
+project = "Alex Clark"
 copyright = '2018 Alex Clark'
 author = 'Alex Clark'
 
