3c800c811892db94173eb2067676739ab97190bb
Alex Clark <aclark@aclark.net>
2018-01-28 14:52:00 -0500

    Update

--- atom.py
+++ atom.py
@@ -1,4 +1,28 @@
 from jinja2 import Template
-atom_xml = open('atom.xml').read()
-template = Template(atom_xml)
-print(template.render(name='Alex Clark'))
+
+template_atom = """
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+
+  <title>Example Feed</title>
+  <link href="http://example.org/"/>
+  <updated>2003-12-13T18:30:02Z</updated>
+  <author>
+    <name>{{ name }}</name>
+  </author>
+  <id>urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6</id>
+
+  <entry>
+    <title>Atom-Powered Robots Run Amok</title>
+    <link href="http://example.org/2003/12/13/atom03"/>
+    <id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
+    <updated>2003-12-13T18:30:02Z</updated>
+    <summary>Some text.</summary>
+  </entry>
+
+</feed>
+"""
+
+template_obj = Template(template_atom)
+template_out = template_obj.render(name='Alex Clark')
+print(template_out)
